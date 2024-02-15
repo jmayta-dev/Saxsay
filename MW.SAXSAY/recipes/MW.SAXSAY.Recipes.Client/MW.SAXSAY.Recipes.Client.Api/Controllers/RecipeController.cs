b8d42d8c6fb3715e44d0baf9142eed1aeaa7170b
Jheison J. Mayta C. <8401842+jmayta-caceres@users.noreply.github.com>
2024-02-14 20:50:33 -0500

    🚧 add client layer

---
+++ MW.SAXSAY/recipes/MW.SAXSAY.Recipes.Client/MW.SAXSAY.Recipes.Client.Api/Controllers/RecipeController.cs
@@ -0,0 +1,35 @@
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using recipes.MW.SAXSAY.Recipes.Application.UseCases.Queries.GetAllQuery;
+using recipes.MW.SAXSAY.Recipes.Application.UseCases.Queries.GetByIdQuery;
+
+namespace recipes.MW.SAXSAY.Recipes.Client.MW.SAXSAY.Recipes.Client.Api.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class RecipeController : ControllerBase
+{
+    private readonly IMediator _mediator;
+
+    public RecipeController(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetAllRecipes()
+    {
+        var response = await _mediator.Send(new GetAllRecipesQuery());
+        return Ok(response);
+    }
+
+    [HttpGet("{recipeId}")]
+    public async Task<IActionResult> GetRecipeById(int recipeId)
+    {
+        var response = await _mediator.Send(new GetRecipeByIdQuery(recipeId));
+        if (response?.Id == null)
+            return NotFound();
+        return Ok(response);
+    }
+}
+
