3138359c0be1af454eeea3bbca58e89d3f5b7343
Jheison J. Mayta C. <8401842+jmayta-caceres@users.noreply.github.com>
2024-04-23 07:15:53 -0500

    add domain base abstract classes

---
+++ src/MW.SAXSAY.Domain/Common/BaseResponse.cs
@@ -0,0 +1,11 @@
+namespace MW.SAXSAY.Domain.Common;
+
+public class BaseResponse<T>
+{
+    public bool IsSuccess { get; set; }
+    public T? Data { get; set; }
+    public string? Message { get; set; }
+    public IEnumerable<BaseError>? Errors { get; set; }
+
+    public BaseResponse() { }
+}
