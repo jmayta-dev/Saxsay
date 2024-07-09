86751ac06733f2a2158dafce097cf1d8d32e2a6c
Jheison J. Mayta C. <8401842+jmayta-dev@users.noreply.github.com>
2024-07-08 21:16:26 -0500

    change test name

--- src/Test/Shared/Helpers/IdentifierGeneratorTests.cs
+++ src/Test/Shared/Helpers/IdentifierGeneratorTests.cs
@@ -6,7 +6,7 @@ namespace MW.SAXSAY.Test.Shared.Helpers;
 public class IdentifierGeneratorTest
 {
     [Fact]
-    public void Time_and_random_based_can_generate_unique_ids_at_high_load()
+    public void Time_plus_random_based_can_generate_unique_ids_at_high_load()
     {
         // Arrange
         HashSet<string> uniqueIds = new();
