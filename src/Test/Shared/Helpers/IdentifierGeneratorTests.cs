ffb87e8d841ce3adba8f4f87617913175f9bd186
Jheison J. Mayta C. <8401842+jmayta-dev@users.noreply.github.com>
2024-07-16 22:32:51 -0500

    add test length for time+random generator

--- src/Test/Shared/Helpers/IdentifierGeneratorTests.cs
+++ src/Test/Shared/Helpers/IdentifierGeneratorTests.cs
@@ -5,6 +5,43 @@ namespace MW.SAXSAY.Test.Shared.Helpers;
 
 public class IdentifierGeneratorTest
 {
+    [Theory]
+    [InlineData(0,24)]
+    [InlineData(1000,24)]
+    [InlineData(6000,24)]
+    [InlineData(10000,24)]
+    [InlineData(30000,24)]
+    public void Time_plus_random_based_ids_same_length_in_any_time(int years, int lengthLimit)
+    {
+        // Arrange
+        string idGenerated = IdentifierGenerators.TimePlusRandomGenerator.Generate(
+            yearsInFuture: years);
+        int actualLength = idGenerated.Length;
+        int expectedLength = lengthLimit;
+        // Act
+        // Assert
+        Assert.Equal(expected:expectedLength, actual:actualLength);
+    }
+
+    [Theory]
+    [InlineData(0,-1,-1,10000)]
+    [InlineData(8,-1,8,10000)]
+    [InlineData(13,13,-1,10000)]
+    [InlineData(15,15,0,10000)]
+    [InlineData(24,15,8,30000)]
+    public void Time_plus_random_based_limited_to_number_of_characters(
+        int lengthLimit, int timestampedLength, int randomLength, int years)
+    {
+        // Arrange
+        string idGenerated = IdentifierGenerators.TimePlusRandomGenerator.Generate(
+            timestampedLength, randomLength, years);
+        int actualLength = idGenerated.Length;
+        int expectedLength = lengthLimit;
+        // Act
+        // Assert
+        Assert.Equal(expectedLength, actualLength);
+    }
+
     [Fact]
     public void Time_plus_random_based_can_generate_unique_ids_at_high_load()
     {
