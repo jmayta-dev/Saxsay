a08bd52f32414398241bdfd48c979487f4c7508e
Jheison J. Mayta C. <8401842+jmayta-dev@users.noreply.github.com>
2024-10-16 23:40:25 -0500

    🚧 add core timestamped ID generator interface

---
+++ src/Core/Application/Contracts/IIdentifierGenerationService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MW.SAXSAY.Core.Application.Contracts;
+
+public interface IIdentifierGenerationService
+{
+    string Generate();
+}
