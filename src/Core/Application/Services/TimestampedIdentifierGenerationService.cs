441c674cbef2a9a5c25c3bbfbfee2f7c8e50be74
Jheison J. Mayta C. <8401842+jmayta-dev@users.noreply.github.com>
2024-10-25 06:11:04 -0500

    add timestamped id generation service

---
+++ src/Core/Application/Services/TimestampedIdentifierGenerationService.cs
@@ -0,0 +1,31 @@
+﻿using MW.SAXSAY.Core.Application.Contracts.Services;
+using MW.SAXSAY.Shared.Helpers;
+
+namespace MW.SAXSAY.Core.Application.Services;
+
+public class TimestampedIdentifierGenerationService : IIdentifierGenerationService
+{
+    public void Dispose()
+    {
+        GC.SuppressFinalize(this);
+    }
+
+    public string Generate(int numericPortionLength = 15, int stringPortionLength = 6)
+    {
+        if (numericPortionLength < 15 || stringPortionLength < 6)
+            throw new ArgumentException(string.Concat(
+                "The length of identifier parts (numeric and string) cannot be less ",
+                "than 15 and 6 respectively."));
+
+        string timestampString =
+            DateTimeOffset
+                .UtcNow
+                .ToUnixTimeMilliseconds()
+                .ToString(new string('0', numericPortionLength));
+
+        string randomString =
+            StringHelpers.RandomString(StringHelpers.PrintableSafe, stringPortionLength);
+
+        return $"{timestampString}{randomString}";
+    }
+}
