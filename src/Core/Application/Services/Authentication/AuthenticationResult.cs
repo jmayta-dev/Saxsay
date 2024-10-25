2928d3f735e7c0e47dcb70333d65ff316e179d71
Jheison J. Mayta C. <8401842+jmayta-dev@users.noreply.github.com>
2024-10-25 06:49:59 -0500

    add authentication service implementation

---
+++ src/Core/Application/Services/Authentication/AuthenticationResult.cs
@@ -0,0 +1,20 @@
+﻿using MW.SAXSAY.Core.Domain.ValueObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MW.SAXSAY.Core.Application.Services.Authentication
+{
+    public record AuthenticationResult(
+        UserId UserId,
+        string Username,
+        string FirstName,
+        string LastName,
+        string Email,
+        bool IsActive,
+        DateTimeOffset DateJoined,
+        Session? Session
+    );
+}
