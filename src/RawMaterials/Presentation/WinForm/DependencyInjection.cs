ecc59a0c77bf43f0704c8f6fcdee24a6166a7afd
Jheison J. Mayta C. <8401842+jmayta-dev@users.noreply.github.com>
2024-11-01 20:22:42 -0500

    add presentation layer service collector

---
+++ src/RawMaterials/Presentation/WinForm/DependencyInjection.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using MW.SAXSAY.RawMaterials.Application;
+using MW.SAXSAY.RawMaterials.Persistence;
+
+namespace MW.SAXSAY.RawMaterials.Presentation.WinForm
+{
+    public static class DependencyInjection
+    {
+        #region Methods
+        //
+        // public
+        //
+        public static IServiceCollection AddRawMaterialServices(this IServiceCollection services)
+        {
+            return services
+                .AddApplicationLayer()
+                .AddPersistenceLayer()
+                .AddPresentationLayer();
+        }
+
+        //
+        // private
+        //
+
+        /// <summary>
+        /// Add forms to the service collection
+        /// </summary>
+        /// <param name="services"></param>
+        /// <returns></returns>
+        private static IServiceCollection AddPresentationLayer(this IServiceCollection services)
+        {
+            return services.AddSingleton<frmRawMaterialManagement>();
+        }
+        #endregion
+    }
+}
