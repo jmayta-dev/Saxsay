48f06bc4e3461f28797d0abca0adaba55fa53d47
Jheison J. Mayta C. <8401842+jmayta-dev@users.noreply.github.com>
2024-11-01 20:19:38 -0500

    add service collector for application layer

---
+++ src/RawMaterials/Application/DependencyInjection.cs
@@ -0,0 +1,17 @@
+using Microsoft.Extensions.DependencyInjection;
+
+namespace MW.SAXSAY.RawMaterials.Application;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddApplicationLayer(this IServiceCollection services)
+    {
+        var applicationAssembly = typeof(DependencyInjection).Assembly;
+
+        services.AddAutoMapper(applicationAssembly);
+        services.AddMediatR(config =>
+            config.RegisterServicesFromAssemblies(applicationAssembly));
+
+        return services;
+    }
+}
