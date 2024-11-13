ac79397570dbd5af4ef765c98dab2af4e759cca5
Jheison J. Mayta C. <8401842+jmayta-dev@users.noreply.github.com>
2024-11-12 21:58:12 -0500

    add controls to raw material management form

--- src/RawMaterials/Presentation/WinForm/frmRawMaterialManagement.Designer.cs
+++ src/RawMaterials/Presentation/WinForm/frmRawMaterialManagement.Designer.cs
@@ -29,6 +29,7 @@
         private void InitializeComponent()
         {
             DataGridViewCellStyle dataGridViewCellStyle1 = new DataGridViewCellStyle();
+            DataGridViewCellStyle dataGridViewCellStyle2 = new DataGridViewCellStyle();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmRawMaterialManagement));
             btnAdd = new Button();
             btnRemove = new Button();
@@ -45,8 +46,11 @@
             colUpdatedAt = new DataGridViewTextBoxColumn();
             colIsEnabled = new DataGridViewCheckBoxColumn();
             mnsMain = new MenuStrip();
-            tsmiFile = new ToolStripMenuItem();
+            tsmiddmFile = new ToolStripMenuItem();
+            tsmiddiSave = new ToolStripMenuItem();
             tsmiSave = new ToolStripMenuItem();
+            toolStripSeparator1 = new ToolStripSeparator();
+            tsmiHistory = new ToolStripMenuItem();
             msSeparator = new ToolStripSeparator();
             tsmiClose = new ToolStripMenuItem();
             ssrStatus = new StatusStrip();
@@ -69,12 +73,12 @@
             // btnAdd
             // 
             btnAdd.Anchor = AnchorStyles.Top | AnchorStyles.Right;
-            btnAdd.Font = new Font("Liberation Sans Narrow", 9F);
+            btnAdd.Font = new Font("Fira Sans Condensed", 9F);
             btnAdd.Image = Properties.Resources.Add;
             btnAdd.ImageAlign = ContentAlignment.MiddleLeft;
-            btnAdd.Location = new Point(673, 6);
+            btnAdd.Location = new Point(769, 6);
             btnAdd.Name = "btnAdd";
-            btnAdd.Size = new Size(84, 26);
+            btnAdd.Size = new Size(85, 26);
             btnAdd.TabIndex = 1;
             btnAdd.Text = "Agregar";
             btnAdd.TextAlign = ContentAlignment.MiddleRight;
@@ -87,9 +91,9 @@
             btnRemove.AutoSize = true;
             btnRemove.Image = Properties.Resources.Remove;
             btnRemove.ImageAlign = ContentAlignment.MiddleLeft;
-            btnRemove.Location = new Point(673, 38);
+            btnRemove.Location = new Point(769, 38);
             btnRemove.Name = "btnRemove";
-            btnRemove.Size = new Size(84, 26);
+            btnRemove.Size = new Size(85, 26);
             btnRemove.TabIndex = 2;
             btnRemove.Text = "Quitar";
             btnRemove.TextAlign = ContentAlignment.MiddleRight;
@@ -100,13 +104,14 @@
             // btnDisable
             // 
             btnDisable.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
+            btnDisable.Font = new Font("Fira Sans Condensed", 9F);
             btnDisable.Image = Properties.Resources.CheckBoxUnchecked;
             btnDisable.ImageAlign = ContentAlignment.MiddleLeft;
-            btnDisable.Location = new Point(673, 359);
+            btnDisable.Location = new Point(769, 359);
             btnDisable.Name = "btnDisable";
-            btnDisable.Size = new Size(84, 26);
+            btnDisable.Size = new Size(85, 26);
             btnDisable.TabIndex = 3;
-            btnDisable.Text = "Deshabilitar";
+            btnDisable.Text = "Deshab.";
             btnDisable.TextAlign = ContentAlignment.MiddleRight;
             btnDisable.TextImageRelation = TextImageRelation.ImageBeforeText;
             btnDisable.UseVisualStyleBackColor = true;
@@ -118,7 +123,7 @@
             tabRawMaterials.Location = new Point(12, 107);
             tabRawMaterials.Name = "tabRawMaterials";
             tabRawMaterials.SelectedIndex = 0;
-            tabRawMaterials.Size = new Size(771, 418);
+            tabRawMaterials.Size = new Size(868, 418);
             tabRawMaterials.TabIndex = 4;
             // 
             // tpRawMaterials
@@ -131,7 +136,7 @@
             tpRawMaterials.Location = new Point(4, 23);
             tpRawMaterials.Name = "tpRawMaterials";
             tpRawMaterials.Padding = new Padding(3);
-            tpRawMaterials.Size = new Size(763, 391);
+            tpRawMaterials.Size = new Size(860, 391);
             tpRawMaterials.TabIndex = 0;
             tpRawMaterials.Text = "Materia Prima";
             tpRawMaterials.UseVisualStyleBackColor = true;
@@ -139,11 +144,12 @@
             // btnEnable
             // 
             btnEnable.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
+            btnEnable.Font = new Font("Fira Sans Condensed", 9F);
             btnEnable.Image = Properties.Resources.CheckBoxChecked;
             btnEnable.ImageAlign = ContentAlignment.MiddleLeft;
-            btnEnable.Location = new Point(673, 327);
+            btnEnable.Location = new Point(769, 327);
             btnEnable.Name = "btnEnable";
-            btnEnable.Size = new Size(84, 26);
+            btnEnable.Size = new Size(85, 26);
             btnEnable.TabIndex = 20;
             btnEnable.Text = "Habilitar";
             btnEnable.TextAlign = ContentAlignment.MiddleRight;
@@ -157,7 +163,7 @@
             dgvRawMaterials.AllowUserToDeleteRows = false;
             dataGridViewCellStyle1.Alignment = DataGridViewContentAlignment.MiddleCenter;
             dataGridViewCellStyle1.BackColor = SystemColors.Control;
-            dataGridViewCellStyle1.Font = new Font("Liberation Sans Narrow", 9F, FontStyle.Bold);
+            dataGridViewCellStyle1.Font = new Font("Fira Sans Condensed", 9F);
             dataGridViewCellStyle1.ForeColor = SystemColors.WindowText;
             dataGridViewCellStyle1.SelectionBackColor = SystemColors.Highlight;
             dataGridViewCellStyle1.SelectionForeColor = SystemColors.HighlightText;
@@ -165,12 +171,23 @@
             dgvRawMaterials.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
             dgvRawMaterials.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             dgvRawMaterials.Columns.AddRange(new DataGridViewColumn[] { colId, colName, colUNSPSC, colUNSPSCDescription, colCreatedAt, colUpdatedAt, colIsEnabled });
-            dgvRawMaterials.EditMode = DataGridViewEditMode.EditOnEnter;
+            dataGridViewCellStyle2.Alignment = DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle2.BackColor = SystemColors.Window;
+            dataGridViewCellStyle2.Font = new Font("JetBrainsMono NF", 9F);
+            dataGridViewCellStyle2.ForeColor = SystemColors.ControlText;
+            dataGridViewCellStyle2.SelectionBackColor = SystemColors.Highlight;
+            dataGridViewCellStyle2.SelectionForeColor = SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = DataGridViewTriState.False;
+            dgvRawMaterials.DefaultCellStyle = dataGridViewCellStyle2;
+            dgvRawMaterials.EditMode = DataGridViewEditMode.EditOnKeystroke;
             dgvRawMaterials.Location = new Point(6, 6);
             dgvRawMaterials.Name = "dgvRawMaterials";
+            dgvRawMaterials.RowHeadersWidth = 25;
             dgvRawMaterials.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
-            dgvRawMaterials.Size = new Size(661, 379);
+            dgvRawMaterials.Size = new Size(757, 379);
             dgvRawMaterials.TabIndex = 19;
+            dgvRawMaterials.CellValueChanged += dgvRawMaterials_CellValueChanged;
+            dgvRawMaterials.CurrentCellDirtyStateChanged += dgvRawMaterials_CurrentCellDirtyStateChanged;
             dgvRawMaterials.KeyPress += dgvRawMaterials_KeyPress;
             // 
             // colId
@@ -180,7 +197,7 @@
             colId.Name = "colId";
             colId.ReadOnly = true;
             colId.ToolTipText = "Identificador";
-            colId.Width = 120;
+            colId.Width = 180;
             // 
             // colName
             // 
@@ -210,15 +227,16 @@
             colCreatedAt.Name = "colCreatedAt";
             colCreatedAt.ReadOnly = true;
             colCreatedAt.ToolTipText = "Fecha de Registro";
-            colCreatedAt.Width = 80;
+            colCreatedAt.Width = 200;
             // 
             // colUpdatedAt
             // 
-            colUpdatedAt.HeaderText = "Fec. Actualización";
+            colUpdatedAt.AutoSizeMode = DataGridViewAutoSizeColumnMode.None;
+            colUpdatedAt.HeaderText = "Fec. Ult. Actualización";
             colUpdatedAt.Name = "colUpdatedAt";
             colUpdatedAt.ReadOnly = true;
-            colUpdatedAt.ToolTipText = "Fecha de Actualización";
-            colUpdatedAt.Visible = false;
+            colUpdatedAt.ToolTipText = "Fecha de Última Actualización";
+            colUpdatedAt.Width = 200;
             // 
             // colIsEnabled
             // 
@@ -229,43 +247,61 @@
             colIsEnabled.Resizable = DataGridViewTriState.True;
             colIsEnabled.SortMode = DataGridViewColumnSortMode.Automatic;
             colIsEnabled.ToolTipText = "Está Activo";
-            colIsEnabled.Width = 20;
+            colIsEnabled.Width = 40;
             // 
             // mnsMain
             // 
-            mnsMain.Font = new Font("Liberation Sans Narrow", 9F);
-            mnsMain.Items.AddRange(new ToolStripItem[] { tsmiFile });
+            mnsMain.Font = new Font("Fira Sans Condensed", 9F);
+            mnsMain.Items.AddRange(new ToolStripItem[] { tsmiddmFile });
             mnsMain.Location = new Point(0, 0);
             mnsMain.Name = "mnsMain";
             mnsMain.Padding = new Padding(5, 2, 0, 2);
-            mnsMain.Size = new Size(795, 24);
+            mnsMain.Size = new Size(892, 24);
             mnsMain.TabIndex = 6;
             // 
-            // tsmiFile
+            // tsmiddmFile
             // 
-            tsmiFile.DropDownItems.AddRange(new ToolStripItem[] { tsmiSave, msSeparator, tsmiClose });
-            tsmiFile.Name = "tsmiFile";
-            tsmiFile.Size = new Size(51, 20);
-            tsmiFile.Text = "Archivo";
+            tsmiddmFile.DropDownItems.AddRange(new ToolStripItem[] { tsmiddiSave, msSeparator, tsmiClose });
+            tsmiddmFile.Name = "tsmiddmFile";
+            tsmiddmFile.Size = new Size(54, 20);
+            tsmiddmFile.Text = "Archivo";
+            // 
+            // tsmiddiSave
+            // 
+            tsmiddiSave.DropDownItems.AddRange(new ToolStripItem[] { tsmiSave, toolStripSeparator1, tsmiHistory });
+            tsmiddiSave.Name = "tsmiddiSave";
+            tsmiddiSave.Size = new Size(180, 22);
+            tsmiddiSave.Text = "Grabar";
             // 
             // tsmiSave
             // 
             tsmiSave.Name = "tsmiSave";
             tsmiSave.ShortcutKeys = Keys.Control | Keys.S;
-            tsmiSave.Size = new Size(176, 22);
-            tsmiSave.Text = "Grabar cambios";
+            tsmiSave.Size = new Size(186, 22);
+            tsmiSave.Text = "Grabar Cambios";
             tsmiSave.Click += tsmiSave_Click;
+            // 
+            // toolStripSeparator1
+            // 
+            toolStripSeparator1.Name = "toolStripSeparator1";
+            toolStripSeparator1.Size = new Size(183, 6);
+            // 
+            // tsmiHistory
+            // 
+            tsmiHistory.Name = "tsmiHistory";
+            tsmiHistory.Size = new Size(186, 22);
+            tsmiHistory.Text = "Historial de cambios...";
             // 
             // msSeparator
             // 
             msSeparator.Name = "msSeparator";
-            msSeparator.Size = new Size(173, 6);
+            msSeparator.Size = new Size(177, 6);
             // 
             // tsmiClose
             // 
             tsmiClose.Name = "tsmiClose";
             tsmiClose.ShortcutKeys = Keys.Control | Keys.Q;
-            tsmiClose.Size = new Size(176, 22);
+            tsmiClose.Size = new Size(180, 22);
             tsmiClose.Text = "Cerrar";
             tsmiClose.Click += tsmiClose_Click;
             // 
@@ -275,7 +311,7 @@
             ssrStatus.Location = new Point(0, 533);
             ssrStatus.Name = "ssrStatus";
             ssrStatus.Padding = new Padding(1, 0, 12, 0);
-            ssrStatus.Size = new Size(795, 22);
+            ssrStatus.Size = new Size(892, 22);
             ssrStatus.TabIndex = 7;
             ssrStatus.Text = "statusStrip1";
             // 
@@ -288,7 +324,7 @@
             // 
             tslblSpace.Font = new Font("Liberation Sans Narrow", 9F);
             tslblSpace.Name = "tslblSpace";
-            tslblSpace.Size = new Size(782, 17);
+            tslblSpace.Size = new Size(879, 17);
             tslblSpace.Spring = true;
             // 
             // tslblContext
@@ -311,7 +347,7 @@
             txtTextToSearch.Location = new Point(6, 35);
             txtTextToSearch.Name = "txtTextToSearch";
             txtTextToSearch.PlaceholderText = "Ingrese texto a buscar";
-            txtTextToSearch.Size = new Size(667, 20);
+            txtTextToSearch.Size = new Size(673, 22);
             txtTextToSearch.TabIndex = 8;
             txtTextToSearch.KeyPress += txtTextToSearch_KeyPress;
             // 
@@ -323,7 +359,7 @@
             grpFilter.Controls.Add(txtTextToSearch);
             grpFilter.Location = new Point(10, 25);
             grpFilter.Name = "grpFilter";
-            grpFilter.Size = new Size(773, 77);
+            grpFilter.Size = new Size(870, 79);
             grpFilter.TabIndex = 9;
             grpFilter.TabStop = false;
             grpFilter.Text = "Filtro";
@@ -333,14 +369,14 @@
             label1.AutoSize = true;
             label1.Location = new Point(6, 18);
             label1.Name = "label1";
-            label1.Size = new Size(52, 14);
+            label1.Size = new Size(58, 14);
             label1.TabIndex = 12;
             label1.Text = "Búsqueda:";
             // 
             // btnSearch
             // 
             btnSearch.Image = Properties.Resources.Search;
-            btnSearch.Location = new Point(679, 32);
+            btnSearch.Location = new Point(775, 32);
             btnSearch.Name = "btnSearch";
             btnSearch.Size = new Size(84, 26);
             btnSearch.TabIndex = 10;
@@ -353,12 +389,12 @@
             // 
             AutoScaleDimensions = new SizeF(6F, 14F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(795, 555);
+            ClientSize = new Size(892, 555);
             Controls.Add(grpFilter);
             Controls.Add(ssrStatus);
             Controls.Add(mnsMain);
             Controls.Add(tabRawMaterials);
-            Font = new Font("Liberation Sans Narrow", 9F);
+            Font = new Font("Fira Sans Condensed", 9F);
             FormBorderStyle = FormBorderStyle.FixedSingle;
             Icon = (Icon)resources.GetObject("$this.Icon");
             MainMenuStrip = mnsMain;
@@ -388,8 +424,7 @@
         private TabPage tpRawMaterials;
         private TabPage tabPage2;
         private MenuStrip mnsMain;
-        private ToolStripMenuItem tsmiFile;
-        private ToolStripMenuItem tsmiSave;
+        private ToolStripMenuItem tsmiddmFile;
         private StatusStrip ssrStatus;
         private ToolStripSeparator msSeparator;
         private ToolStripMenuItem tsmiClose;
@@ -402,6 +437,11 @@
         private Button btnSearch;
         private Label label1;
         private DataGridView dgvRawMaterials;
+        private Button btnEnable;
+        private ToolStripMenuItem tsmiddiSave;
+        private ToolStripMenuItem tsmiHistory;
+        private ToolStripMenuItem tsmiSave;
+        private ToolStripSeparator toolStripSeparator1;
         private DataGridViewTextBoxColumn colId;
         private DataGridViewTextBoxColumn colName;
         private DataGridViewTextBoxColumn colUNSPSC;
